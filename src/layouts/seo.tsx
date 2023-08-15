2b11615c6cc3a20b81af33583d6acbee07be47ef
vitaxa <vitaxa93gamebox@gmail.com>
2023-08-15 14:12:29 +0300

    fix seo

--- src/layouts/seo.tsx
+++ src/layouts/seo.tsx
@@ -21,8 +21,8 @@ const SEO: FC<IProps> = ({ title, titleTemplate, description }) => {
 
 SEO.defaultProps = {
     title: "MarketDB",
-    titleTemplate: "Аналитика маркетплейса KazanExpress",
-    description: "Аналитика маркетплейса KazanExpress",
+    titleTemplate: "Аналитика маркетплейса Uzum",
+    description: "Аналитика маркетплейса Uzum",
 };
 
 export default SEO;
